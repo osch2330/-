37100ff014e0d21229e1337730ac66abb5495e11
osch2330 <73734204+osch2330@users.noreply.github.com>
2020-10-31 18:34:41 +0900

    Update 식단1.py

--- 식단1.py
+++ 식단1.py
@@ -4,4 +4,4 @@
 import telegram
 토큰 = "1460256695:AAFbMqwAwPs7Wd8qR3BhO0syOFxRzvocRZQ"
 봇 = telegram.Bot(token=토큰)
-봇.sendMessage("1473034824","오늘은 급식이 없네요...")
+봇.sendMessage("1452976504","오늘은 급식이 없네요...")
